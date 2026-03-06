99cf68d043d1929a29598e8caaa612ba383443b3
robbyd <robby.darmawan89@gmail.com>
2026-03-06 14:12:59 +0700

    Fix external resource

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,8 +7,8 @@
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <!-- <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> -->
         
         <!-- Preload Custom Fonts -->
         <link rel="preload" href="/fonts/Raverist.woff2" as="font" type="font/woff2" crossorigin>
